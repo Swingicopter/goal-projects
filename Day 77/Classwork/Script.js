a9c12a3ba7479d00a46e22a892039062d309f400
Swingicopter <filipebur@icloud.com>
2025-03-13 20:50:53 +0400

    commit

---
+++ Day 77/Classwork/Script.js
@@ -0,0 +1,14 @@
+function borderRadius(){
+    let image = document.getElementById('poster')
+    image.style.borderRadius = '15px'
+}
+
+function boxShadow(){
+    let image = document.getElementById('poster')
+    image.style.boxShadow = '5px 5px 10px 2px rgba(0, 0, 0, 0.75)'
+}
+
+function changePicture(){
+    let image = document.getElementById('poster')
+    image.src = 'monitor.png'
+}
---
+++ Day 77/Homework/Script.js
@@ -0,0 +1,19 @@
+body = document.getElementById("body")
+image = document.getElementById()
+
+function changeBackgroundColor(){
+    body.style.backgroundColor = "black"
+}
+
+function changeButtonColor(){
+    console.log("ver gavige")
+}
+
+function appealTheme(){
+    body.style.backgroundColor = "rgb(13, 13, 13)"
+    image.src = "GOA.png"
+}
+
+function changeImage(){
+    image.src = "OIG3.png"
+}
