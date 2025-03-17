9050664336d9a3562e23a5056227f104584d93b5
Swingicopter <filipebur@icloud.com>
2025-03-17 20:32:23 +0400

    59th commit

---
+++ Day 78/Homework/Script.js
@@ -0,0 +1,5 @@
+let color = prompt("Type your name")
+let welcomeMat = document.getElementById("welcomeMat")
+
+welcomeMat.textContent = "Hello, " + name
+alert("Hello, " + name)
---
+++ Day 78/Homework/Script2.js
@@ -0,0 +1,3 @@
+let color = prompt("What's your favourite color?")
+
+alert("Your favourite color is " + color)
