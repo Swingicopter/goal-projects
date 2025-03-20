b623f5877476c5b07a10a648020372ef80ddeb15
Swingicopter <filipebur@icloud.com>
2025-03-20 20:20:35 +0400

    60th commit

---
+++ Day 79/Classwork/Script.js
@@ -0,0 +1,5 @@
+let prompty = prompt("Type something")
+let confirmBox = confirm("You get another chance to type something")
+let prompty2 = prompt("Type something again")
+alert("Your input: " + prompty2)
+console.log(prompty2)
---
+++ Day 79/Homework/Script.js
@@ -0,0 +1,18 @@
+conscent = confirm("Stay on site?")
+
+if (conscent = true) {
+    alert("You're staying on the site.")
+}
+else{
+    alert("You're leaving the site.")
+}
+
+a = "yes"
+b = 2.34
+c = 9
+d = true
+e = b + c
+f = a + String(d) + String(e)
+
+console.log(f)
+console.log(2 < 5)
